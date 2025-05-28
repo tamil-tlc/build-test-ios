b9af5efd2ade82b1c9d534b37f84fb3a2582e852
Tamil <127006077+tamil-tlc@users.noreply.github.com>
2025-05-28 12:42:28 +0530

    Update build_test_iosTests.swift

--- build-test-iosTests/build_test_iosTests.swift
+++ build-test-iosTests/build_test_iosTests.swift
@@ -1,10 +1,6 @@
 import Testing
 @testable import build_test_ios
-
 struct build_test_iosTests {
-
     @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
 }
