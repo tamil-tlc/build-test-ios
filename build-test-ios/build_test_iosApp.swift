1641ef79659d6f431956e43f249fad176bd702ef
Tamil <127006077+tamil-tlc@users.noreply.github.com>
2025-05-28 10:18:23 +0530

    Update build_test_iosApp.swift

--- build-test-ios/build_test_iosApp.swift
+++ build-test-ios/build_test_iosApp.swift
@@ -1,12 +1,4 @@
-//
-//  build_test_iosApp.swift
-//  build-test-ios
-//
-//  Created by Anbu p on 28/05/25.
-//
-
 import SwiftUI
-
 @main
 struct build_test_iosApp: App {
     var body: some Scene {
